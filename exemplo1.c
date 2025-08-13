fb501742282644b5e48fa2f3beb06e62a7a27c2e
kauareis26 <kauareis336@gmail.com>
2025-08-12 22:08:50 -0300

    Create exemplo1.c

---
+++ exemplo1.c
@@ -0,0 +1,17 @@
+/*
+ * Lab. 1 
+ * Exemplo 1
+ */
+ 
+ #include <stdio.h>
+ int main()  {
+ 	// imprime algumas mensagens na tela
+ 	printf("Mackenzie - FCI\n");
+ 	printf("Algoritimos e ");
+ 	printf("Prog. II\n");
+ 	printf("Turma 02.P");
+ 	// pausa abtes de terminar o programa 
+ 	system("pause");
+ 	return 0 ;
+ 	
+ }
